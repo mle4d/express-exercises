13d40ced92efe6ad4e16d02ecd57c84b9acaf07b
emily <mle4d@me.com>
2019-07-10 17:18:30 -0700

    single color stuff, maybe

--- public/js/main.js
+++ public/js/main.js
@@ -6,7 +6,7 @@ fetch('/api/v1/colors')
   .then(colors => {
     colors.forEach(color => {
       const li = document.createElement('li');
-      li.textContent = `${color.name}`;
+      li.textContent = `${color.name} ${color.hex} ${color.r} ${color.g} ${color.b}`;
       colorList.appendChild(li);
     });
   });
